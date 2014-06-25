5fb3f7e99b64b9cc85b5f45691cad8967e0b469d
Michael Storm <mstorm7@gmail.com>
2014-06-25 15:08:53 -0700

    Added include guards to md5 header file

--- src/md5.h
+++ src/md5.h
@@ -1,6 +1,11 @@
+#ifndef DJ_MD5_H
+#define DJ_MD5_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <openssl/md5.h>
 
 int file_md5(uint32_t inode, char *path, uint64_t pos, uint64_t file_len,
 			 char *data, uint64_t data_len, void **private);
+
+#endif
