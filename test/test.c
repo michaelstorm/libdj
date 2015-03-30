29bd181b5070d24549b6df1894ec947c34ea49f0
Michael Storm <mstorm7@gmail.com>
2015-03-29 20:13:04 -0700

    Convert test_scan_block body to a macro, for some reason I don't remember

--- test/test.c
+++ test/test.c
@@ -7,48 +7,48 @@ int nop_cb(uint32_t inode, char *path, uint64_t pos, uint64_t file_len,
     return 0;
 }
 
-void test_scan_block()
-{
-    uint64_t block_size = 4096;
-    blk64_t physical_block = 17;
-    e2_blkcnt_t logical_block = 2;
-    int file_len = 10343250;
-
-    struct inode_cb_info inode_info = {
-        .len = file_len,
-    };
-
-    struct block_list block_list = {
-        .inode_info = &inode_info,
-        .physical_block = physical_block,
-        .logical_block = logical_block,
-        .next = NULL,
-    };
-
-    struct inode_list inode_list = {
-        .len = file_len,
-        .next = NULL,
-        .blocks_start = NULL,
-        .blocks_end = NULL,
-    };
-
-    struct scan_blocks_info scan_info = {
-        .cb = nop_cb,
-        .inode_info = &inode_info,
-        .inode_list = &inode_list,
-    };
-
-    scan_block(block_size, physical_block, logical_block, &scan_info);
-
-    fprintf(stdout, "inodes: ");
-    print_inode_list(stdout, &inode_list);
-    fprintf(stdout, "\nblocks: ");
-    print_block_list(stdout, inode_list.blocks_start);
+#define TEST_SCAN_BLOCK(_physical_block, _logical_block, _file_len) \
+    uint64_t block_size = 1024; \
+    \
+    struct inode_cb_info inode_info = { \
+        .len = _file_len, \
+    }; \
+    \
+    struct block_list block_list = { \
+        .inode_info = &inode_info, \
+        .physical_block = _physical_block, \
+        .logical_block = _logical_block, \
+        .next = NULL, \
+    }; \
+    \
+    struct inode_list inode_list = { \
+        .len = _file_len, \
+        .next = NULL, \
+        .blocks_start = NULL, \
+        .blocks_end = NULL, \
+    }; \
+    \
+    struct scan_blocks_info scan_info = { \
+        .cb = nop_cb, \
+        .inode_info = &inode_info, \
+        .inode_list = &inode_list, \
+    }; \
+    \
+    scan_block(block_size, _physical_block, _logical_block, &scan_info); \
+    \
+    fprintf(stdout, "inodes: "); \
+    print_inode_list(stdout, &inode_list); \
+    fprintf(stdout, "\nblocks: "); \
+    print_block_list(stdout, inode_list.blocks_start); \
     fprintf(stdout, "\n");
+
+void test_scan_first_block()
+{
+    TEST_SCAN_BLOCK(17, 2, 3498734)
 }
 
 int main(int argc, char **argv)
 {
-    test_scan_block();
+    test_scan_first_block();
     return 1;
 }
