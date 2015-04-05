a79d199547e3345e4d01741e8ccc8e2286a5949d
Michael Storm <mstorm7@gmail.com>
2015-04-05 16:02:31 -0700

    Add first draft of noise-creation script

---
+++ test/noise.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+import envoy
+import os.path
+import random
+import subprocess
+import sys
+import time
+import uuid
+
+MEGABYTE = 1048576
+
+
+def run_command(command, print_debug=True, check_error=True):
+    if print_debug:
+        sys.stdout.write('Running %s... ' % command)
+        sys.stdout.flush()
+
+    r = envoy.run(command)
+    if check_error and r.status_code != 0:
+        raise Exception('Error running "%s": exited with code %d, error: %s', command, r.status_code, r.std_err)
+
+    if print_debug:
+        if r.status_code == 0:
+            sys.stdout.write('succeeded\n')
+        else:
+            sys.stdout.write('failed\n')
+        sys.stdout.flush()
+
+    return r
+
+
+def run_commands(commands, **kwargs):
+    for command in commands:
+        run_command(command, **kwargs)
+
+
+def create_local_fs(size_kb, block_device, data_path, mount_path):
+    dd_cmd = 'dd if=/dev/zero of="%s" bs=1024 count=%d' % (data_path, size_kb)
+    losetup_cmd = 'losetup %s %s' % (block_device, data_path)
+    mkfs_cmd = 'mkfs.ext4 %s' % block_device
+    mkdir_cmd = 'mkdir -p %s' % mount_path
+    mount_cmd = 'mount %s "%s"' % (block_device, mount_path)
+    commands = [dd_cmd, losetup_cmd, mkfs_cmd, mkdir_cmd, mount_cmd]
+    run_commands(commands)
+
+
+def destroy_local_fs(block_device, data_path, mount_path):
+    mount_cmd = 'umount "%s"' % mount_path
+    losetup_cmd = 'losetup -d %s' % block_device
+    rm_cmd = 'rm %s' % data_path
+    commands = [mount_cmd, losetup_cmd, rm_cmd]
+    run_commands(commands, check_error=False)
+
+
+def get_files(root_path):
+    files = None
+    for _, _, f in os.walk(root_path):
+        files = f
+        break
+
+    return files
+
+
+def write_file(root_path, size):
+    path = os.path.join(root_path, str(uuid.uuid4()))
+    with open(path, 'w') as f:
+        #print('Writing file %s of %d bytes' % (path, size))
+        f.write('1' * size)
+
+
+def write_random_files(root_path, min_bytes, max_bytes, count):
+    for i in range(count):
+        size = random.randint(min_bytes, max_bytes)
+        write_file(root_path, size)
+
+
+def delete_random_file(root_path):
+    files = get_files(root_path)
+
+    if len(files) > 0:
+        filename = random.choice(files)
+        path = os.path.join(root_path, filename)
+
+        #print('Deleting %s' % path)
+        os.remove(path)
+        return True
+    else:
+        print('No files to delete')
+        return False
+
+
+def write_until_usage(block_device, min_file_bytes, max_file_bytes, target_usage_ratio):
+    fs_info = get_fs_info(block_device)
+    usage = fs_info[0] * 1024
+    available = fs_info[1] * 1024
+    root_path = fs_info[2]
+
+    total = usage + available
+    target_usage = int(total * target_usage_ratio)
+    print('total: %d, target_usage: %d, current: %d' % (total, target_usage, usage))
+
+    if usage < target_usage:
+        while usage < target_usage - 10000:
+            file_size = random.randint(min_file_bytes, max_file_bytes)
+            write_file(root_path, file_size)
+            usage = get_fs_info(block_device)[0] * 1024
+    else:
+        while usage > target_usage + 10000:
+            deleted = delete_random_file(root_path)
+            usage = get_fs_info(block_device)[0] * 1024
+            if not deleted:
+                break
+
+    #print('Ending with differential of %d bytes' % (target_usage - usage))
+
+
+def get_fs_info(block_device):
+    r = run_command('df %s' % block_device, print_debug=False)
+    line = r.std_out.split('\n')[1].split()
+    return int(line[2]), int(line[3]), line[5]
+
+
+def run_command_streaming_stdout(args):
+    def print_line(line):
+        sys.stdout.write(line.decode('ascii'))
+        sys.stdout.flush()
+
+    p = subprocess.Popen(args, stdout=subprocess.PIPE)
+
+    while p.poll() is None:
+        line = p.stdout.readline()
+        print_line(line)
+
+    line = p.stdout.read()
+    print_line(line)
+
+
+block_device = run_command('losetup -f').std_out.strip()
+data_path = 'test.ext4'
+mount_path = '/media/test.ext4'
+
+try:
+    create_local_fs(100000, block_device, data_path, mount_path)
+
+    for i in range(1):
+        target_usage_ratio = random.random()
+        write_until_usage(block_device, 1000, MEGABYTE, target_usage_ratio)
+
+    run_command_streaming_stdout(['../build/src/dj_cmd', block_device, '/'])
+
+finally:
+    # sleep to ensure disk is not busy when we try to unmount it
+    time.sleep(1)
+    destroy_local_fs(block_device, data_path, mount_path)
